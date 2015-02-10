827f1ad46d7291f82c7efa703381ee512fcc4416
1006198k <1006198k@bo620-23u>
2015-02-10 19:24:40 +0000

    added partitioner

---
+++ src/task2/MyPartitioner.java
@@ -0,0 +1,20 @@
+package task2;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+
+
+
+public class MyPartitioner extends Partitioner<LongWritable, IntWritable>{
+	
+	private long ID = 15070781;
+	
+	public int getPartition(LongWritable key, IntWritable value, int numPartitions){
+		
+		long keyid = key.get();
+		
+		return (int)Math.floor((float)((keyid + 0.0)/ ID) * numPartitions);
+	}
+
+}
