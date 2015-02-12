b140ad5c0546d96fb352b4ce2031500de77740db
1006198k <1006198k@bo620-10u>
2015-02-12 15:23:16 +0000

    added k-part in sortreducer

--- src/task2/SortReducer.java
+++ src/task2/SortReducer.java
@@ -11,10 +11,16 @@ import org.apache.hadoop.mapreduce.Reducer.Context;
 @SuppressWarnings("unused")
 public class SortReducer extends Reducer< ArtCountPair,  IntWritable, LongWritable, Text>{
 	
+	static int count = 0;
+	
 	public void reduce(ArtCountPair key, Iterable< IntWritable> values, Context context) throws IOException, InterruptedException{
 		 
-		
-		context.write(new LongWritable(key.getArticle()), new Text(String.valueOf(key.getCount())));
+		int limit = context.getConfiguration().getInt("k", 0);
+		if (count < limit){
+			context.write(new LongWritable(key.getArticle()), new Text(String.valueOf(key.getCount())));
+			count ++;
+			
+		}
 	}
 
 }
